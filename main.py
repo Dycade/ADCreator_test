5f4404489597556cb8d967b05c75d2508c384b0e
Guochao Sun <85650434+Dycade@users.noreply.github.com>
2022-06-13 04:04:59 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+class Load_settings:
+    def __init__(self):
+        file = open("AD_settings.txt", 'r')
+        self.whole_settings = file.read().split('\n\n')
+        base_dict = {}
+        for line in self.whole_settings[0].split('\n'):
+            if '=' in line:
+                k, v = line.strip().split('=')
+                base_dict[k.strip()] = v.strip()
+        self.setting = base_dict
+    def change(self, i):
+        temp ={}
+        for line in self.whole_settings[i].split('\n'):
+            if '=' in line:
+                k, v = line.strip().split('=')
+                temp[k.strip()] = v.strip()
+        self.setting = temp
+
+
+AD_settings = Load_settings()
+setting_dict = AD_settings.setting
+
+if __name__ == '__main__':
+    if setting_dict['Security_level'] == 'Low':
+         exec(open("ADCreatorLow.py").read())
+    else:
+         exec(open("ADCreator.py").read())
+
+
+
+
+
+
